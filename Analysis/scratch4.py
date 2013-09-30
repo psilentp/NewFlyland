d914ad13a64b5474661c24417f85977e3938e6a9
Theodore Lindsay <psilentp@yahoo.com>
2013-09-29 17:11:43 -0700

    FUNCTIONAL: started working on general code for wingbeats. Wrote a function to find wingbeat peaks - better and faster than find_peaks_cwt. Integrate into general code and maybe run when constructing the FlyRecord objet to create a new signal at init (the medfilt will take a while to run on the whole dataset but will provide a better signal than sync for wbf.

--- Analysis/scratch4.py
+++ Analysis/scratch4.py
@@ -5,4 +5,16 @@ def plot_lowspikes(spiketrains):
     for st in spiketrains:
         lowind = np.argwhere(st.annotations['phases']>4)
         [plb.plot(st.waveforms[x], color = 'k',alpha = 0.5) for x in lowind[:20]]
-    
+
+def get_wingbeats(wb_signal):
+    thresh = 0.5
+    from scipy.signal import medfilt
+    detrend = np.array(wb_signal)-medfilt(wb_signal,151)
+    deltas = np.diff(np.array(detrend>0.5,dtype = 'float'))
+    starts = np.argwhere(deltas>0.5)
+    stops = np.argwhere(deltas<-0.5)
+    if starts[0] > stops[0]:
+        stops = stops[1:]
+    intervals = np.hstack((starts,stops))
+    peaks = [np.argmax(wb_signal[sta:stp])+sta for sta,stp in intervals]
+    return peaks
