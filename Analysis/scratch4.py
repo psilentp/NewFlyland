0decaf496fcdd947d8c390825fe85d4635ca9fe2
Theodore Lindsay <psilentp@yahoo.com>
2013-09-30 00:14:07 -0700

    FUNCTIONAL: Playing around a little more with PCA and EM methods

--- Analysis/scratch4.py
+++ Analysis/scratch4.py
@@ -10,7 +10,20 @@ def get_wingbeats(wb_signal):
     thresh = 0.5
     from scipy.signal import medfilt
     detrend = np.array(wb_signal)-medfilt(wb_signal,151)
-    deltas = np.diff(np.array(detrend>0.5,dtype = 'float'))
+    deltas = np.diff(np.array(detrend>thresh,dtype = 'float'))
+    starts = np.argwhere(deltas>0.5)
+    stops = np.argwhere(deltas<-0.5)
+    if starts[0] > stops[0]:
+        stops = stops[1:]
+    intervals = np.hstack((starts,stops))
+    peaks = [np.argmax(wb_signal[sta:stp])+sta for sta,stp in intervals]
+    return peaks
+    
+def get_spikepeaks(signal):
+    thresh = 10.0
+    from scipy.signal import medfilt
+    detrend = np.array(signal)-medfilt(signal,21)
+    deltas = np.diff(np.array(detrend>thresh,dtype = 'float'))
     starts = np.argwhere(deltas>0.5)
     stops = np.argwhere(deltas<-0.5)
     if starts[0] > stops[0]:
