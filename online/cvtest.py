a1c10e7b84c69fd3efba82cf349a9b87c508c2f7
psilentp <psilentp@yahoo.com>
2013-10-08 14:18:00 -0700

    AVT mod

--- online/cvtest.py
+++ online/cvtest.py
@@ -1,19 +1,27 @@
 import cv2
+from pyVimba import VimbaCamera, pyVimbaShutdown
 
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
+cams = VimbaCamera.getAvailableCameras(True)
+cam = VimbaCamera(cams[0]['id'])
+cam.grabStart()
+
+
+if cam.getFeature("AcquisitionStart"): # try to get the first frame
+    frame = cam.getImage(2000)
+    rval = True
 else:
     rval = False
 
 while rval:
     cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    frame = cv2.Laplacian(frame,5)
+    frame = cam.getImage(2000)
+    #frame = cv2.Laplacian(frame,5)
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
     
 cv2.destroyWindow('preview')
+pyVimbaShutdown()
