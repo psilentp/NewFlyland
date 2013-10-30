dd718bf022acb3d817f7dd07e5e3be88b5a88879
Theodore Lindsay <psilentp@yahoo.com>
2013-10-29 17:54:27 -0700

    Wrote a data model in flyphys.py also started working on a controller that extracts wb signal data and the preliminary spike train pool.

---
+++ Analysis/flyphys.py
@@ -0,0 +1,296 @@
+#script to parse the data for a single fly - load % cut up the .abf and matlab format and save the mean traces in a python friendly format.
+import os
+import neo
+import scipy.io as sio
+import quantities as pq
+from pylab import *
+import numpy as np
+from scipy.signal import medfilt,find_peaks_cwt
+import pylab as plb
+
+from signal_tools import *
+
+#fly_number = 1
+#rep_ind = 0
+dataroot = '/Users/psilentp/Dropbox/Data/FirstEphys'
+
+azmuth_lookup = {5:90,11:-90}
+L_V_lookup = {1:5,2:20,3:50,4:100,5:200}
+
+##transform to convert expansion x signal into degrees
+pattern_xnum = 768
+deg_per_xstp = 360./pattern_xnum
+ol_volts_per_deg = 10.0/(pattern_xnum*deg_per_xstp)
+cl_volts_per_rad = 5/(np.pi)
+expan_transform = lambda x:x/ol_volts_per_deg
+fix_transform = lambda x:x/cl_volts_per_rad
+
+class FlyRecord(object):
+    """data model for a single ephys recording, trial times
+    and types are encoded in the .mat file associated with the .abf file"""
+    def __init__(self,
+                fly_number,
+                axon_file_path,
+                mat_file_path,
+                clepoch =  pq.Quantity(5,'s'),
+                olepoch = pq.Quantity(1.2,'s'),
+                min_wbf = pq.Quantity(150.0,'Hz'),
+                exp_note_path=None,
+                post_process_path = None):
+                
+        self.fly_number = fly_number
+        self.axon_file_path = axon_file_path
+        self.mat_file_path = mat_file_path
+        self.exp_note_path = exp_note_path
+        self.post_process_path = post_process_path
+        self.clepoch = clepoch
+        self.olepoch = olepoch
+        self.tail = pq.Quantity(.4,'s')
+        self.processed_signals = dict()
+        self.processing_params = dict()
+        self.load_data()
+        
+        self.left_window = int(self.clepoch/self.dt)
+        self.right_window = int(self.olepoch/self.dt + self.tail/self.dt)
+        self.min_wbf = min_wbf
+        
+    def load_data(self):
+        importer = neo.io.AxonIO(filename = self.axon_file_path)
+        block = importer.read_block()
+        hdr = importer.read_header()
+        #holds the asignals
+        seg = block.segments[0]
+        #get and sort the trial presentation record
+        trial_matrix = sio.loadmat(self.mat_file_path)['datarecord'][0]
+        #now add an index to the first column.
+        self.trial_matrix = np.array([[i,x[0][0][0],x[1][0][0]] for i,x in enumerate(trial_matrix)])
+        #sort the data: first by presentation order, then by function type, then by position.
+        self.sorted_trial_indices = lexsort((self.trial_matrix[:,0],self.trial_matrix[:,1],self.trial_matrix[:,2]))
+        #make a lookup dictionary for the signal indicies
+        self.signals = dict()
+        for index,sig_name in enumerate(h['ADCChNames'] for h in hdr['listADCInfo']):
+            self.signals[sig_name] = seg.analogsignals[index]
+        #pull out the trial start indicies
+        self.dt = self.signals['Sync'].times[1]
+        self.trial_start_indicies = where(diff(where(self.signals['Ypos'] <1,1,0))>0)[0]
+    
+    def __getitem__(self,k):
+        """allow slicing by according to: 
+        self[function_index,position_index,trial_num,'SignalString'] 
+        fix: make sure that we sqeeze out a singelton list"""
+        function_index = k[0]
+        position_index = k[1]
+        sli = k[2]
+        sigkey = k[3]
+        retlist = list()
+        selected_trials_indices = [rps[0] for rps in self.trial_matrix if rps[1] == function_index and rps[2] == position_index][sli]
+        if not(type(selected_trials_indices) is list):
+            selected_trials_indices = [selected_trials_indices]
+        for x in selected_trials_indices:
+            #first get the section presumed to include t-collision
+            start_ind = self.trial_start_indicies[x]-self.left_window
+            end_ind = self.trial_start_indicies[x]+self.right_window
+            #if self.get_wbf(start_ind,end_ind) > self.min_wbf:
+            #shift the start and end ind to correct for the actual collision time.
+            icol = where(self.signals['Xpos'][self.trial_start_indicies[x]+5000:end_ind] >= ol_volts_per_deg*89.5)[0][0]
+            offset = icol + 5000 - floor(int(self.olepoch/self.dt)) - 1
+            start_ind += offset
+            end_ind += offset
+            retlist.append(self.signals[sigkey][start_ind:end_ind])
+        for sig in retlist:
+            #set the start time correcting for rounding errors
+            sig.t_start = -1*(int(self.clepoch/self.dt)+int(self.olepoch/self.dt)+1)*self.dt#-2*(self.epoch)
+        return retlist
+    
+    def get_wbf(self,start_ind,end_ind):
+        duration = (end_ind-start_ind)*self.signals['Sync'].times[1]
+        wingbeats = len(where(diff(where(self.signals['Sync'][start_ind:end_ind]<1,1,0))>0.5)[0])
+        freq =  wingbeats/duration
+        return freq
+     
+    def calculate_average(self, 
+                          signal_key, 
+                          function_index, 
+                          position_index,
+                          ave_range,
+                          transform = None):
+        #siglist = self.__getitem__((signal_key,function_index,position_index))
+        siglist = self[function_index,position_index,:,signal_key]
+        st = np.argwhere(siglist[0].times<ave_range[0])[-1]
+        en = np.argwhere(siglist[0].times<ave_range[1])[-1]
+        if transform:
+            siglist = [transform(x[st:en]) for x in siglist]
+        else:
+            siglist = [x[st:en] for x in siglist]
+        ave = reduce(lambda x,y:x+y,siglist)/len(siglist)  
+        return ave
+
+class FlyController(object):
+    
+    def __init__(self,fly):
+        self.fly = fly
+        self.verbose = True
+         
+    def process_wb_signals(self):
+        from scipy.signal import hilbert
+        pts_per_wb = 0.005/self.fly.dt #for 200 Hz wb
+        L_h = self.fly.signals['LeftWing']
+        R_h = self.fly.signals['RightWing']
+        arr = L_h+R_h
+        if self.verbose:print('calculating flight mask')
+        flight_mask = window_stdev(L_h+R_h,int(5*pts_per_wb)) < 0.5 #window apx 5 wb
+        if self.verbose:print('calculating stroke phase')
+        phases = np.angle(hilbert(get_low_filter(L_h+R_h,500)))
+        if self.verbose:print('calculating wb reference features')
+        pks = get_wingbeats(L_h+R_h)
+        newpks,flips = recondition_peaks(L_h+R_h,pks)
+        phase_shift = np.mean(phases[flips])
+        if self.verbose:print('re-wraping phase')
+        phases = np.mod(np.unwrap(phases)-phase_shift,2*np.pi)
+        L_a = np.zeros_like(L_h)
+        R_a = np.zeros_like(R_h)
+        p0 = newpks[0]
+        if self.verbose:print('calculating stroke amplitudes')
+        for pk in newpks[1:]:
+            L_a[p0:pk] = L_h[p0]
+            R_a[p0:pk] = R_h[p0]
+            p0 = pk
+        L_a[0:newpks[0]] = L_h[0]
+        R_a[0:newpks[0]] = R_h[0]
+        L_a[newpks[-1]:] = L_h[newpks[-1]]
+        R_a[newpks[-1]:] = R_h[newpks[-1]]
+        if self.verbose:print('updating processed_signals dict')
+        self.fly.processed_signals['wb_phase'] = phases
+        self.fly.processed_signals['wb_peaks'] = newpks
+        self.fly.processed_signals['wb_flips'] = flips
+        self.fly.processed_signals['flight_mask'] = flight_mask
+        self.fly.processed_signals['LeftAmp'] = L_a
+        self.fly.processed_signals['RightAmp'] = R_a
+    
+    def extract_spike_pool(self):
+        """extract the pool of potential spikes"""
+        try:
+            phys_sig = self.fly.signals['AMsysCh1']
+            thresh = self.fly.processing_params['spk_pool_thresh']
+            wl= self.fly.processing_params['spk_window_left']
+            wr= self.fly.processing_params['spk_window_right']
+            filter_window = self.fly.processing_params['spk_pool_filter_window']
+            spike_pool = get_spiketrain(phys_sig,
+                                    thresh = thresh,
+                                    wl=wl,
+                                    wr=wr,
+                                    filter_window = filter_window)
+        self.fly.processed_signals['spike_pool'] = spike_pool
+        
+
+def get_fly_in_rootdir(data_root,fly_number,rep_ind):
+        data_dir = data_root + '/Fly%s'%(fly_number)
+        data_files = os.listdir(data_dir)
+        abf_files = [f for f in data_files if '.abf' in f]
+        mat_files = [f for f in data_files if '.mat' in f]
+        exp_note_files = [f for f in data_files if '.txt' in f]
+        post_process_files = [f for f in data_files if '.cpkl' in f]
+        
+        axon_file_path = data_dir + '/' + abf_files[rep_ind]
+        mat_file_path = data_dir + '/' + mat_files[rep_ind]
+        try:
+            exp_note_path = data_dir + '/' + exp_note_files[rep_ind]
+        except IndexError:
+            exp_note_path = None
+        try:
+            post_process_path = data_dir + '/' + post_process_files[rep_ind]
+        except IndexError:
+            post_process_path = None
+        axon_file_path,mat_file_path,exp_note_path,post_process_path
+        fly = FlyRecord(fly_number,
+                        axon_file_path = axon_file_path,
+                        mat_file_path = mat_file_path,
+                        exp_note_path = exp_note_path,
+                        post_process_path = post_process_path)
+        return fly
+        
+        
+        
+
+def sort_spikes(wv_mtrx,M):
+    from scipy.linalg import svd
+    from scipy.cluster.vq import kmeans2
+    import sklearn
+    from sklearn import cluster, datasets
+    from sklearn.metrics import euclidean_distances
+    from sklearn.neighbors import kneighbors_graph
+    from sklearn.preprocessing import StandardScaler
+    from sklearn import svm
+    from sklearn.covariance import EllipticEnvelope
+    import pywt
+    from scipy import stats
+    
+    p2p = np.max(wv_mtrx,axis = 1) -np.min(wv_mtrx,axis = 1)
+    p2pt = np.argmax(wv_mtrx,axis = 1) - np.argmin(wv_mtrx,axis = 1)
+    
+    
+    ##medsweep erro
+    wv_med = np.median(wv_mtrx,axis = 0)
+    err_vec = np.sum(np.sqrt((wv_mtrx-wv_med)**2),axis = 1)
+    err_vec /= np.max(err_vec)
+    print(shape(err_vec))
+    print(shape(p2p))
+    
+    wave_dff = np.diff(wv_mtrx,axis = 1)
+    
+    
+    dp2p = np.max(wave_dff,axis = 1) -np.min(wave_dff,axis = 1)
+    dp2pt = np.argmax(wave_dff,axis = 1) - np.argmin(wave_dff,axis = 1)
+    #print(shape(wv_mtrx))
+    
+    
+    wtr = vstack([hstack(pywt.wavedec(wv_mtrx[x,:],'db9',level = 3)) for x in range(shape(wv_mtrx)[0])])
+    wtr2 = vstack([hstack(pywt.wavedec(wv_mtrx[x,:],'haar',level = 3)) for x in range(shape(wv_mtrx)[0])])
+    #wv_mtrx = vstack([pywt.dwt(wv_mtrx[x,:],'db2')[1] for x in range(shape(wv_mtrx)[0])])
+    #print(shape(wv_mtrx))
+    wv_mean = np.mean(wv_mtrx)
+    datamtrx = wv_mtrx-wv_mean
+    U,s,Vt = svd(datamtrx,full_matrices=False)
+    V = Vt.T
+
+    ind = np.argsort(s)[::-1]
+    U = U[:,ind]
+    s = s[ind]
+    V = V[:,ind]
+    #print shape(wtr)
+    #print(shape(array([p2p]).T))
+    #print(shape(U))
+    #print shape(wtr)
+    #features = np.concatenate((wtr,array([p2p]).T,array([p2pt]).T,array([dp2p]).T,array([dp2pt]).T,U),axis = 1)
+    #features = np.concatenate((array([p2p]).T,array([p2pt]).T,array([dp2p]).T,array([dp2pt]).T,U),axis = 1)
+    features = np.concatenate((array([p2p]).T,array([err_vec]).T,array([p2pt]).T,wtr[:,:3],U[:,:3]),axis = 1)
+    #features = np.concatenate((array([p2p]).T,array([p2pt]).T,wtr[:,:3],U[:,:3]),axis = 1)
+    #dbscan = cluster.DBSCAN(eps=1.0)
+    X = StandardScaler().fit_transform(features[:,:2])
+    
+    #outliers_fraction = 0.25
+    #clsf = svm.OneClassSVM(nu=0.95 * outliers_fraction + 0.05,
+    #                                 kernel="rbf", gamma=0.1)
+    
+    
+                                            
+           
+    #X = features[:,:5]
+    #clsf.fit(X[:,:2])
+    #y_pred = clsf.decision_function(X).ravel()
+    #threshold = stats.scoreatpercentile(y_pred,
+    #                                        100 * outliers_fraction)
+    #idx = y_pred > threshold
+    #dbscan.fit(X[:,:5])
+    #idx = dbscan.labels_.astype(np.int)
+    
+    #features = Ur
+    #print(shape(features))
+    #print shape(U)
+    #M = 2
+    #M = np.array([[-0.72394844,0.6905216 ],[ 0.50694304,-0.48353598]])
+    es, idx = kmeans2(X[:,:4],M)
+    print es
+    #es, idx = kmeans2(features[:,:3],2,minit = 'points')
+    #es, idx = kmeans2(features[:,:8],4)
+    return idx,features,U
---
+++ Analysis/signal_tools.py
@@ -0,0 +1,143 @@
+import os
+import neo
+import scipy.io as sio
+import quantities as pq
+from pylab import *
+import numpy as np
+from scipy.signal import medfilt,find_peaks_cwt
+import pylab as plb
+
+
+def ts(sweep,start,stop):
+    sta_index = argwhere(sweep.times >=start)[0]
+    stp_index = argwhere(sweep.times >=stop)[0]
+    return neo.AnalogSignal(sweep[sta_index:stp_index],
+                            t_start = sweep.times[sta_index],
+                            sampling_rate = sweep.sampling_rate)
+
+def window_stdev(arr, window):
+    from scipy.ndimage.filters import uniform_filter
+    c1 = uniform_filter(arr, window*2, mode='constant', origin=-window)
+    c2 = uniform_filter(arr*arr, window*2, mode='constant', origin=-window)
+    return ((c2 - c1*c1)**.5)
+
+
+def recondition_peaks(signal,pks):
+    newpks = list()
+    flips = list()
+    for pk in pks:
+        offset = np.argmax(signal[pk-10:pk+10])
+        newpks.append(pk-10+offset)
+        try:
+            flip = argwhere( (diff(signal[pk-20:pk])>0) < 1 )[-1]+1
+            flips.append(pk-20+int(flip))
+        except IndexError:
+            pass
+    return newpks,flips
+        
+def get_spiketrain(sweep,thresh = 10,wl=25,wr=20,filter_window = 35):
+    from scipy.signal import medfilt
+    detrend = np.array(sweep)-medfilt(sweep,filter_window)
+    deltas = np.diff(np.array(detrend>thresh,dtype = 'float'))
+    starts = np.argwhere(deltas>0.5)
+    stops = np.argwhere(deltas<-0.5)
+    if starts[0] > stops[0]:
+        stops = stops[1:]
+    if stops[-1] < starts[-1]:
+        starts = starts[:-1]
+    intervals = np.hstack((starts,stops))
+    peaks = [np.argmax(sweep[sta:stp])+sta for sta,stp in intervals]
+    waveforms = [sweep[pk-wl:pk+wr] for pk in peaks]
+    sweep.sampling_period.units = 's'
+    pk_tms = sweep.times[array(peaks)]
+    spike_train = neo.SpikeTrain(pk_tms,
+                                sweep.t_stop,
+                                sampling_rate = sweep.sampling_rate,
+                                waveforms = waveforms,
+                                left_sweep = wl*sweep.sampling_period,
+                                t_start = sweep.t_start,
+                                pk_ind = peaks)
+    return spike_train
+
+
+def get_wingbeats(wb_signal,filter_window = 151,thresh = 0.5):
+    from scipy.signal import medfilt
+    detrend = np.array(wb_signal)-medfilt(wb_signal,filter_window)
+    deltas = np.diff(np.array(detrend>thresh,dtype = 'float'))
+    starts = np.argwhere(deltas>0.5)
+    stops = np.argwhere(deltas<-0.5)
+    if starts[0] > stops[0]:
+        stops = stops[1:]
+    if stops[-1] < starts[-1]:
+        starts = starts[:-1]
+    intervals = np.hstack((starts,stops))
+    peaks = [np.argmax(wb_signal[sta:stp])+sta for sta,stp in intervals]
+    return peaks
+    
+def get_signal_mean(signal_list):
+    """calculate the average signal from a list of signals"""
+    #print signal_list
+    mean_signal = reduce(lambda x,y:x+y,signal_list)/len(signal_list)
+    return mean_signal
+
+def get_signal_stderr(signal_list,signal_mean = None):
+    """return the standard error of a group of signals"""
+    if signal_mean is None:
+        signal_mean = get_signal_mean(signal_list)
+    mean_sq_err = get_signal_mean(map(lambda x: (x-signal_mean)**2,signal_list))
+    #print sqrt(mean_sq_err)/sqrt(len(signal_list))
+    rsig = sqrt(mean_sq_err)/sqrt(len(signal_list))
+    rsig.sampling_period = signal_list[0].sampling_period
+    rsig.t_start = signal_list[0].t_start
+    return rsig
+
+def get_low_filter(signal,Hz):
+    #return a low-pass-filterd version of the time series
+    from scipy.signal import filtfilt
+    filter_order = 3
+    sampfreq = signal.sampling_rate
+    filtfreq = pq.Quantity(Hz,'Hz')
+    passband_param = float((filtfreq/sampfreq).simplified)
+    from scipy.signal import butter
+    [b,a]=butter(filter_order,passband_param)
+    #ob = cp.copy(signal)
+    filt_array = filtfilt(b,a,signal)
+    #ob.y = quan.Quantity(filtfilt(b,a,self.y),self.yunits)
+    return neo.AnalogSignal(filt_array,
+                            units = signal.units,
+                            sampling_rate = signal.sampling_rate,
+                            t_start = signal.t_start)
+        
+def calculate_groupwise_means(flylist = []):
+    import cPickle as cpkl
+    group_matrix = dict()
+    average_matrix = dict()
+    stde_matrix = dict()
+    #flylist = [1,2,3,4,5,6,7,9,10,11,12]
+    for findex in range(5):
+            for pindex in[4,10]:
+                group_matrix['Xpos',findex+1,pindex+1] = list()
+                group_matrix['L_m_R',findex+1,pindex+1] = list()
+                average_matrix['Xpos',findex+1,pindex+1] = list()
+                average_matrix['L_m_R',findex+1,pindex+1] = list()  
+                stde_matrix['Xpos',findex+1,pindex+1] = list()
+                stde_matrix['L_m_R',findex+1,pindex+1] = list()
+    #load the group matrix with fly data
+    for flynum in flylist:
+        fi = open('fly%s.cpkl'%(flynum),'rb')
+        fly_data = cpkl.load(fi)
+        print("loading fly%s"%(flynum))
+        for findex in range(5):
+            for pindex in[4,10]:
+                group_matrix['Xpos',findex+1,pindex+1].append(fly_data['Xpos',findex+1,pindex+1])
+                group_matrix['L_m_R',findex+1,pindex+1].append(fly_data['L_m_R',findex+1,pindex+1])
+    for findex in range(5):
+        for pindex in [4,10]:
+            print('averaging cell %s %s'%(findex,pindex))
+            average_matrix['Xpos',findex+1,pindex+1] = get_signal_mean(group_matrix['Xpos',findex+1,pindex+1])
+            average_matrix['L_m_R',findex+1,pindex+1] = get_signal_mean(group_matrix['L_m_R',findex+1,pindex+1])
+            stde_matrix['Xpos',findex+1,pindex+1] = get_signal_stderr(group_matrix['Xpos',findex+1,pindex+1],signal_mean = average_matrix['Xpos',findex+1,pindex+1])
+            stde_matrix['L_m_R',findex+1,pindex+1] = get_signal_stderr(group_matrix['L_m_R',findex+1,pindex+1],signal_mean = average_matrix['L_m_R',findex+1,pindex+1])
+    return average_matrix,stde_matrix,group_matrix
+
+
